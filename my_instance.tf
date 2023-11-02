f8f3a432468ab5f20764a2d26631ae69e5a0dda2
Akhil-Afzal <akhil.afzal@gmail.com>
2023-11-03 00:35:32 +0530

    tags are added to resourse section

--- my_instance.tf
+++ my_instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 	region = "eu-north-1"
-	access_key = "AKIAQIM4V7WUWFQZATEO"
-    secret_key = "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"
+	access_key = "AKIAQIM4V7WUQPBLHOKH"
+	secret_key = "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"
 }
 
 resource "aws_instance" "intro" {
@@ -9,9 +9,10 @@ resource "aws_instance" "intro" {
 	instance_type = "t3.micro"
 	availability_zone = "eu-north-1a"
 	key_name = "Dove-key"
-	vpc_security_groups_ids = [sg-01b80a056a94c1ce6]
+	vpc_security_group_ids = ["sg-01b80a056a94c1ce6"]
 	tags = {
 	  Name = "Dove-instances"
+	  Project = "Dove"
 	} 
 
 }
